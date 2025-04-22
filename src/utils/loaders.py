fe696437fb637f78d9f33ff16ddb458c1d79f12b
oscar <oscar.reves@gmail.com>
2025-04-22 12:26:55 +0200

    Added print statements to loaders

--- src/utils/loaders.py
+++ src/utils/loaders.py
@@ -1,19 +1,25 @@
 from datasets import load_dataset
 import json
 
-def load_raw_articles(path):
+def load_raw_articles(path, silent=False):
     # for loading raw articles after scraping
     dataset = load_dataset("json", data_files=path, split='train')
     dataset = dataset.filter(lambda x: x['error'] is None) # filter away articles where scraping failed
+    if not silent:
+        print(f"{len(dataset)} documents loaded")
     return dataset
 
-def load_documents(path):
+def load_documents(path, silent=False):
     # for loading chunked documents
     dataset = load_dataset("json", data_files=path, split='train')
+    if not silent:
+        print(f"{len(dataset)} documents loaded")
     return dataset
 
-def load_questions(path):
+def load_questions(path, silent=False):
     dataset = load_dataset("json",data_files=path, field=None, split='train')
+    if not silent:
+        print(f"{len(dataset)} questions loaded")
     return dataset
 
 def save_as_json(data, path):
