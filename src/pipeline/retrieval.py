3c7c70fc897f7acc914c58bc3da722870793c50c
oscar <oscar.reves@gmail.com>
2025-04-25 15:30:12 +0200

    Ready to test retrieval with actual squad question on cluster lets gooooo

--- src/pipeline/retrieval.py
+++ src/pipeline/retrieval.py
@@ -1,4 +1,5 @@
 import json
+import tqdm 
 
 def test_retrieval(question_dataset, retriever, save_path, batch_size = 16):
     results = []
@@ -11,14 +12,17 @@ def test_retrieval(question_dataset, retriever, save_path, batch_size = 16):
             } 
         results.append(result)
     
+    
+
     print(f"Saving {len(results)} results to {save_path}")
     with open(save_path, 'w') as fp:
         json.dump(results, fp, indent=2, ensure_ascii=False)
 
+
 def test_retrieval_with_uid(question_dataset, retriever, save_path, batch_size = 16):
     print(f"Testing uid based retrieval...")
     results = []
-    for sample in question_dataset:
+    for i, sample in enumerate(tqdm(question_dataset)):
         question = sample['question']
         context = retriever.retrieve_titles_with_uid([question])
         result = {
@@ -26,6 +30,8 @@ def test_retrieval_with_uid(question_dataset, retriever, save_path, batch_size =
             "context"   :   context
             } 
         results.append(result)
+        if i == 99:
+            break
     
     print(f"Saving {len(results)} results to {save_path}")
     with open(save_path, 'w') as fp:
