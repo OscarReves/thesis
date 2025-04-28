caa922952bce8517caaacdb73bf46d6078bf2c87
oscar <oscar.reves@gmail.com>
2025-04-28 14:15:47 +0200

    Added batching to test_qa_with_retrieval_wiki

--- src/pipeline/qa_with_retrieval.py
+++ src/pipeline/qa_with_retrieval.py
@@ -52,3 +52,31 @@ def test_qa_with_retrieval_wiki(question_dataset, retriever, generator, save_pat
     print(f"Saving {len(results)} results to {save_path}")
     with open(save_path, 'w') as fp:
         json.dump(results, fp, indent=2, ensure_ascii=False) 
+
+def test_qa_with_retrieval_wiki(question_dataset, retriever, generator, save_path, batch_size = 16, max_samples=100):
+    # still needs batching 
+    question_dataset=question_dataset.select(range(max_samples))
+    results = []
+    for i in (tqdm(range(0, len(question_dataset), batch_size), 
+                   desc=f"Answering questions in batches of {batch_size}")):
+        batch = question_dataset[i:i+batch_size]
+        questions = batch['question']
+        contexts = retriever.retrieve([questions])
+        
+        answers = generator.generate_bnatch(questions,contexts)
+        
+        # this method for retrieving the answer needs to be generalized across datasets somehow
+        reference_answers = batch['answers'] 
+
+        result = [{
+            "question"          :   q,
+            "context"           :   c,
+            "generated_answer"  :   a,
+            "reference_answer"  :   ra
+            } for q, c, a, ra in zip(questions,contexts,answers, reference_answers)]
+        
+        results.append(result)
+    
+    print(f"Saving {len(results)} results to {save_path}")
+    with open(save_path, 'w') as fp:
+        json.dump(results, fp, indent=2, ensure_ascii=False) 
