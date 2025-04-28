2755c209a0565ce78347a0313c0d3227442665b8
oscar <oscar.reves@gmail.com>
2025-04-28 11:14:24 +0200

    Added retrieval test with batching

--- src/pipeline/retrieval.py
+++ src/pipeline/retrieval.py
@@ -35,20 +35,18 @@ def test_retrieval_with_uid(question_dataset, retriever, save_path, max_samples=
     with open(save_path, 'w') as fp:
         json.dump(results, fp, indent=2, ensure_ascii=False)
 
-def test_batched_retrieval_with_uid(question_dataset, retriever, save_path, batch_size = 16):
+def test_batched_retrieval_with_uid(question_dataset, retriever, save_path, batch_size = 16, max_samples=1600):
     print(f"Testing uid based retrieval with batching...")
     results = []
-    for i, sample in enumerate(tqdm(question_dataset)):
-        question = sample['question']
-        context = retriever.retrieve_titles_with_uid([question])
-        result = {
-            "question"  :   question,
-            "context"   :   context
-            } 
-        results.append(result)
-        if i == 99:
-            break
-    
+    for i in (tqdm(range(0, len(question_dataset), batch_size), desc="Retrieving context")):
+        batch = question_dataset[i:i+batch_size]
+        questions = batch['question']
+        contexts = retriever.retrieve_titles_with_uid([questions])
+        results.extend([
+            {"question": q, "contexts": c}
+            for q,c in zip(questions,contexts)
+        ])
+
     print(f"Saving {len(results)} results to {save_path}")
     with open(save_path, 'w') as fp:
         json.dump(results, fp, indent=2, ensure_ascii=False)
--- src/utils/batch_iterator.py
+++ src/utils/batch_iterator.py
@@ -3,6 +3,7 @@ import math
 
 def batch_iterator(dataset, batch_size):
     # Load an HF dataset in batches
+    # This is apparently unnecessary, you can simply use slice indexing and avoid out of bounds errors
     total_batches = math.ceil(len(dataset) / batch_size)
     for i in tqdm(range(0, len(dataset), batch_size), total=total_batches):
         yield dataset.select(range(i, min(i + batch_size, len(dataset))))
