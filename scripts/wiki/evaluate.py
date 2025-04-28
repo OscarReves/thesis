0670a56e5ca774a4ddc01a9d6f5c9a50a0116657
oscar <oscar.reves@gmail.com>
2025-04-28 13:21:28 +0200

    Added eval for wiki answers

---
+++ scripts/wiki/evaluate.py
@@ -0,0 +1,31 @@
+import yaml
+import argparse
+from src.utils import load_documents
+from src.evaluator import get_evaluator
+from src.pipeline import evaluate_answers
+
+def main(config_path):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+
+    answers_path = config['answers_path']
+    evaluator_name = config['evaluator_name']
+    save_path = config['evaluation_path']
+    batch_size = config['eval_batch_size']
+
+    answers = load_documents(answers_path)
+    evaluator = get_evaluator(evaluator_name)
+
+    evaluate_answers(
+        answer_dataset = answers,
+        evaluator = evaluator,
+        save_path = save_path,
+        batch_size=batch_size
+        )
+
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", required=True, help="Path to config YAML")
+    args = parser.parse_args()
+    main(args.config)
