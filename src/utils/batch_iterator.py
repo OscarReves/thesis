c5b447b56a3c51b71a7465be060564d4070149d9
oscar <oscar.reves@gmail.com>
2025-04-30 10:42:21 +0200

    fixed description parameter in batch_iterator

--- src/utils/batch_iterator.py
+++ src/utils/batch_iterator.py
@@ -5,5 +5,5 @@ def batch_iterator(dataset, batch_size, description=None):
     # Load an HF dataset in batches
     # This is apparently unnecessary, you can simply use slice indexing and avoid out of bounds errors
     total_batches = math.ceil(len(dataset) / batch_size)
-    for i in tqdm(range(0, len(dataset), batch_size), total=total_batches, description=description):
+    for i in tqdm(range(0, len(dataset), batch_size), total=total_batches, desc=description):
         yield dataset.select(range(i, min(i + batch_size, len(dataset))))
