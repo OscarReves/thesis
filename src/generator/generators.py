97a758f4e5edefc94a7a2aaef7a4e650f17a601c
oscar <oscar.reves@gmail.com>
2025-05-01 10:59:11 +0200

    Added question disambiguation to generator

--- src/generator/generators.py
+++ src/generator/generators.py
@@ -103,3 +103,47 @@ class NousHermesMistral2Generator():
             for i, input_len in enumerate(input_lengths)
         ]
         return outputs
+
+    def rewrite_questions_in_batch(self, questions, contexts, max_new_tokens=256):
+        system_prompt = (
+            "You are a helpful assistant. You respond to prompts in Danish. "
+            "Respond briefly and accurately. Do not generate any extra questions or superfluous text. "
+            "Be as concise as possible."
+        )
+
+        prompts = [
+            self.tokenizer.apply_chat_template([
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": f"""
+                You are an expert question question disambiguator.
+                Using the context, rewrite the following question so that it would be unambiguous in the absence of the context:
+                 
+                Kontekst: {c}
+                 
+                Spørgsmål: {q}
+                """}
+            ], tokenize=False, add_generation_prompt=True)
+            for q, c in zip(questions, contexts)
+        ]
+
+        inputs = self.tokenizer(prompts, return_tensors="pt", padding=True, truncation=True).to(self.model.device)
+
+        with torch.inference_mode():
+            output_ids = self.model.generate(
+                **inputs,
+                max_new_tokens=max_new_tokens,
+                do_sample=False,
+                num_beams=1,
+                use_cache=True,
+                #temperature=0.7,
+                #top_p=0.9,
+                eos_token_id=self.tokenizer.convert_tokens_to_ids("<|im_end|>"),
+                pad_token_id=self.tokenizer.eos_token_id
+            )
+
+        input_lengths = [len(i) for i in inputs["input_ids"]]
+        outputs = [
+            self.tokenizer.decode(output_ids[i][input_len:], skip_special_tokens=True).strip()
+            for i, input_len in enumerate(input_lengths)
+        ]
+        return outputs
