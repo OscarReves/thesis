5fd1f44fe4ebe55c6b3aa4f65b4b60bfabe3609c
oscar <oscar.reves@gmail.com>
2025-05-01 12:56:23 +0200

    Fixed batch_iterator to correctly subset with max_samples

--- src/utils/batch_iterator.py
+++ src/utils/batch_iterator.py
@@ -7,5 +7,5 @@ def batch_iterator(dataset, batch_size, description=None, max_samples=None):
     if not max_samples:
         max_samples=len(dataset)
     total_batches = math.ceil(max_samples / batch_size)
-    for i in tqdm(range(0, len(dataset), batch_size), total=total_batches, desc=description):
+    for i in tqdm(range(0, max_samples, batch_size), total=total_batches, desc=description):
         yield dataset.select(range(i, min(i + batch_size, len(dataset))))
