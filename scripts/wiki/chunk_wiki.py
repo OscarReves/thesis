7340e7f4c03fb96c3c0f1ea5c48afa5eeb9d63b0
oscar <oscar.reves@gmail.com>
2025-05-01 15:52:45 +0200

    Added print statement

--- scripts/wiki/chunk_wiki.py
+++ scripts/wiki/chunk_wiki.py
@@ -12,15 +12,15 @@ def main(config_path):
         config = yaml.safe_load(f)
 
     dump_path = config["dump_path"] # path to parent directory containing wiki files from dump
-    #tokenizer_name = config["tokenizer_name"]
-    tokenizer_name = None
+    tokenizer_name = config["tokenizer_name"]
     out_path = config["chunked_path"]
 
     # iterate through all wiki files
+    print(f"Chunking files in {dump_path} and saving to {out_path}...")
     chunk_multiple(
         dump_dir=dump_path, 
         out_dir=out_path,
-        tokenizer_name=tokenizer_name,  # implement this 
+        tokenizer_name=None,  # implement this 
     )
 
 if __name__ == "__main__":
