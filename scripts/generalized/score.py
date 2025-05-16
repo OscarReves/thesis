63e5541934cb25dab0400c45669d5d0a7bb67d87
oscar <oscar.reves@gmail.com>
2025-05-16 13:50:06 +0200

    Ready to run multiple tests with Gemma

--- scripts/generalized/score.py
+++ scripts/generalized/score.py
@@ -11,8 +11,12 @@ def main(config_path):
     evaluation_type = config['evaluation_type']
     accuracy = get_accuracy(answers, type=evaluation_type)
     print(f" {answers_path} Accuracy: {accuracy:.2f}")
-
     
+    # Append to log file
+    with open("results/accuracy.txt", "a") as log_file:
+        log_file.write(f"{answers_path}\tAccuracy: {accuracy:.3f}\n")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", required=True, help="Path to config YAML")
--- src/generator/factory.py
+++ src/generator/factory.py
@@ -1,9 +1,10 @@
-from .generators import NousHermesMistralGenerator, SuzumeLlama3Generator, Yi34BGenerator
+from .generators import NousHermesMistralGenerator, SuzumeLlama3Generator, Yi34BGenerator, Gemma9bGenerator
 
 GENERATOR_REGISTRY = {
     "nous-hermes-mistral"   : NousHermesMistralGenerator,
     "suzume-llama3"         : SuzumeLlama3Generator,
-    "yi-34b"                : Yi34BGenerator
+    "yi-34b"                : Yi34BGenerator,
+    "gemma-9b"              : Gemma9bGenerator
 }
 
 def get_generator(name, **kwargs):
