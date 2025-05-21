e54f4ef24b4873a6576cde44a943e284268bc755
oscar <oscar.reves@gmail.com>
2025-05-21 11:38:40 +0200

    Fixed param

--- scripts/tests/bm25_test.py
+++ scripts/tests/bm25_test.py
@@ -3,7 +3,7 @@ from src.utils import load_documents
 
 def main():
     documents = load_documents('/dtu/p1/oscrev/news/2025/chunked_paragraph.jsonl')
-    bm25_retriever = get_retriever('bm25', index=None, documents=documents)
+    bm25_retriever = get_retriever('bm25', index_path=None, documents=documents)
     question = "Hvem nænvnte kong Frederik i sin nytårstale?"
     results = bm25_retriever.retrieve([question])
     print(f"Retrieved contexts: {results}")
