c702e3c2ced58256bb28cab2b70119f372044666
oscar <oscar.reves@gmail.com>
2025-05-22 22:27:25 +0200

    Added news-compatible mc qa pipeline

--- src/pipeline/qa_with_retrieval.py
+++ src/pipeline/qa_with_retrieval.py
@@ -166,6 +166,33 @@ def qa_citizenship_mc(question_dataset, retriever, generator, save_path,
     
     save_to_json(results, save_path, result_type="answers with context")
 
+def qa_news_mc(question_dataset, retriever, generator, save_path, 
+                                batch_size=16, silent=True, max_samples=None):
+    if max_samples:
+        question_dataset = question_dataset.select(range(max_samples))
+    results = []
+    
+    for i in tqdm(range(0, len(question_dataset), batch_size), 
+                  desc=f"Answering questions in batches of {batch_size}"):
+        batch = question_dataset[i:i+batch_size]
+        questions = batch['question']
+        options = batch['options']
+
+        contexts = retriever.retrieve(questions)
+
+        answers = generator.generate_batch_mc(questions, contexts, options)
+        
+        reference_answer = batch['mc_answer']
+
+        results.extend([{
+            "question"         : q,
+            "context"          : c,
+            "generated_answer" : a,
+            "reference_answer" : ra
+        } for q, c, a, ra in zip(questions, contexts, answers, reference_answer)])
+    
+    save_to_json(results, save_path, result_type="answers with context")
+
 def qa_citizenship_mc_no_context(question_dataset, retriever, generator, save_path, 
                                 batch_size=16, silent=True, max_samples=None):
     if max_samples:
@@ -189,3 +216,4 @@ def qa_citizenship_mc_no_context(question_dataset, retriever, generator, save_pa
         } for q, a, ra in zip(questions, answers, reference_answer)])
     
     save_to_json(results, save_path, result_type="answers with context")
+
