7c46be3a58fb30fa2b1b71d005b5e34731d75637
oscar <oscar.reves@gmail.com>
2025-05-23 17:28:59 +0200

    Added multiple evaluation

---
+++ scripts/experiments/evaluate_multiple.py
@@ -0,0 +1,40 @@
+import yaml
+import argparse
+from src.utils import load_documents
+from src.evaluator import get_evaluator
+from src.pipeline import evaluate_answers
+from pathlib import Path
+
+def main(config_path):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+
+    answers_dir = config['answers_dir']
+    save_dir = config['save_dir']
+    evaluator_name = 'gemma-9b'
+    save_path = config['evaluation_path']
+    batch_size = config['batch_size']
+    evaluator = get_evaluator(evaluator_name)
+
+    # iterate through directory 
+    directory = Path(answers_dir)
+
+    for file in directory.iterdir():
+        answers_path = str(file)
+        answers = load_documents(answers_path)
+
+        save_path = save_dir / file.name
+
+        evaluate_answers(
+            answer_dataset = answers,
+            evaluator = evaluator,
+            save_path = save_path,
+            batch_size=batch_size
+            )
+
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", required=True, help="Path to config YAML")
+    args = parser.parse_args()
+    main(args.config)
