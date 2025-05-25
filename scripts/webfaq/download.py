84eff9e2d183521fce77691989e1a759ff996039
oscar <oscar.reves@gmail.com>
2025-05-25 19:25:51 +0200

    (4)

--- scripts/webfaq/download.py
+++ scripts/webfaq/download.py
@@ -4,7 +4,7 @@ def main():
     # Stream the Danish subset and take the first 100 examples
     dataset = load_dataset("PaDaS-Lab/webfaq", "dan", split='default')
     # Take the first 100 examples
-    dataset.save_to_disk("/dtu/p1/webfaq_danish")
+    dataset.save_to_disk("/dtu/p1/oscrev/webfaq_danish")
 
 
 if __name__ == "__main__":
