bfaf17bea67cf9518942c0c3777c593a38d69243
oscar <oscar.reves@gmail.com>
2025-05-25 19:23:43 +0200

    (2)

--- scripts/webfaq/download.py
+++ scripts/webfaq/download.py
@@ -1,23 +1,11 @@
-import yaml
-from src.utils import load_knowledge_base, load_questions_by_type
-from src.retriever import get_retriever
-from src.generator import get_generator
-from src import pipeline as pipeline_module
-import argparse 
+from datasets import load_dataset
 
 def main(config_path):
-    with open(config_path, "r") as f:
-        config = yaml.safe_load(f)
-
-    # in the future clean this up with the double asteriks ** method
-    generator_name = config['generator_name']
-
-    print("Loading generator...")
-    generator = get_generator(generator_name)
+    # Stream the Danish subset and take the first 100 examples
+    dataset = load_dataset("PaDaS-Lab/webfaq", "dan", split='default')
+    # Take the first 100 examples
+    dataset.save_to_disk("/dtu/p1/webfaq_danish")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", required=True, help="Path to config YAML")
-    args = parser.parse_args()
-    main(args.config)
+    main()
