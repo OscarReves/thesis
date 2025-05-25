455008b3d4d2615c60350d2e2da63cee196aafa1
oscar <oscar.reves@gmail.com>
2025-05-25 19:24:27 +0200

    (3)

--- scripts/webfaq/download.py
+++ scripts/webfaq/download.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 
-def main(config_path):
+def main():
     # Stream the Danish subset and take the first 100 examples
     dataset = load_dataset("PaDaS-Lab/webfaq", "dan", split='default')
     # Take the first 100 examples
