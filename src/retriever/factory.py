787c811f857718f4968122b0d2f3e027f08d9181
oscar <oscar.reves@gmail.com>
2025-05-25 22:03:48 +0200

    Ready to test with gpu-based retrieval

--- src/retriever/factory.py
+++ src/retriever/factory.py
@@ -1,7 +1,8 @@
-from .retrievers import E5Retriever, DummyRetriever, BM25Retriever, SparseBM25Retriever
+from .retrievers import E5Retriever, DummyRetriever, BM25Retriever, SparseBM25Retriever, E5RetrieverGPU
 
 RETRIEVER_REGISTRY = {
     "e5"    : E5Retriever,
+    "e5_gpu" : E5RetrieverGPU,
     "dummy" : DummyRetriever,
     "bm25"  : BM25Retriever,
     "bm25-sparse" : SparseBM25Retriever
