1715e3fe591f0795ec787068bace2a357b06a459
oscar <oscar.reves@gmail.com>
2025-05-26 12:21:31 +0200

    (2)

--- src/embedder/embedders.py
+++ src/embedder/embedders.py
@@ -99,10 +99,9 @@ class E5EmbedderLocal(E5Embedder):
         #super().__init__(device, model_name)
         if 'cuda' in device and not torch.cuda.is_available():
             raise RuntimeError(f"CUDA requested but not available on this system (device={device})")
+        self.device = device
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModel.from_pretrained("intfloat/multilingual-e5-large").to(device)
-        self.device = device
-
         # load weights of saved model
         state_dict = torch.load(save_path)
         # because of dual-gpu training, state_dict needs to be refactored 
@@ -110,7 +109,7 @@ class E5EmbedderLocal(E5Embedder):
         self.model.load_state_dict(new_state_dict)
         #model.to("cuda")
         self.model.eval()
-        torch.set_float32_matmul_precision('high') # is this necessary?
+        #torch.set_float32_matmul_precision('high') # is this necessary?
 
 class BertTinyEmbedder:
     def __init__(self, device, model_name='prajjwal1/bert-tiny'):
