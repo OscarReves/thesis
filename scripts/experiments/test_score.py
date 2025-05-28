1a51b18094fc884ee828f31a301a6334f68f8e6f
oscar <oscar.reves@gmail.com>
2025-05-28 14:22:10 +0200

    (4)

--- scripts/experiments/test_score.py
+++ scripts/experiments/test_score.py
@@ -2,6 +2,7 @@ import yaml
 import argparse
 from src.utils import load_documents, get_accuracy
 from pathlib import Path
+import os 
 
 def main(config_path):
     with open(config_path, "r") as f:
@@ -24,17 +25,19 @@ def main(config_path):
         with open("results/accuracy.txt", "a") as log_file:
             log_file.write(f"{answers_path}\tAccuracy: {accuracy:.3f}\n")
 
-    for mc_file in multiple_choice_dir.iterdir():
-        answers_path = str(mc_file)
-        answers = load_documents(answers_path)
-        evaluation_type = 'multiple_choice'
-        accuracy = get_accuracy(answers, type=evaluation_type)
-        print(f" {answers_path} Accuracy: {accuracy:.2f}")
-        
-        # Append to log file
-        with open("results/accuracy.txt", "a") as log_file:
-            log_file.write(f"{answers_path}\tAccuracy: {accuracy:.3f}\n")
-    
+    if os.path.exists(multiple_choice_dir):
+        for mc_file in multiple_choice_dir.iterdir():
+            answers_path = str(mc_file)
+            answers = load_documents(answers_path)
+            evaluation_type = 'multiple_choice'
+            accuracy = get_accuracy(answers, type=evaluation_type)
+            print(f" {answers_path} Accuracy: {accuracy:.2f}")
+            
+            # Append to log file
+            with open("results/accuracy.txt", "a") as log_file:
+                log_file.write(f"{answers_path}\tAccuracy: {accuracy:.3f}\n")
+    else:
+        print(f"No multiple choice answers found in {dir}")
 
 
 
