d08f390e0c1b9bc40c5332278471eb03696b27e2
oscar <oscar.reves@gmail.com>
2025-05-29 20:47:02 +0200

    YOU CHANGED THE SYSTEM PROMPT FOR GEMMA. REMEMBER TO CHANGE IT BACK

--- src/generator/generators.py
+++ src/generator/generators.py
@@ -244,11 +244,17 @@ class Gemma9bGenerator(BaseGenerator):
 
     # requires its own prompt formatting since the 'system' role is not supported in chat_template    
     def generate_batch(self, questions, contexts, max_new_tokens=32):
+        # system_prompt = (
+        #     "You are a helpful assistant. You respond to questions in Danish. "
+        #     "Respond briefly and accurately. Do not generate any extra questions or superfluous text. "
+        #     "Be as concise as possible."
+        #     "The context may or may not be relevant."
+        # )
+
         system_prompt = (
             "You are a helpful assistant. You respond to questions in Danish. "
             "Respond briefly and accurately. Do not generate any extra questions or superfluous text. "
             "Be as concise as possible."
-            "The context may or may not be relevant."
         )
 
         prompts = [
