22a63af31d669bc9f6e4bef1e4ecf4a9379fcc6b
oscar <oscar.reves@gmail.com>
2025-06-01 22:05:23 +0200

    Trying HF upload again

--- scripts/hendrix/upload_to_HF.py
+++ scripts/hendrix/upload_to_HF.py
@@ -7,7 +7,7 @@ import os
 def main():
     
     # Load model
-    save_path = 'models/e5_finetuned_epoch7.pt'
+    save_path = 'models/e5_finetuned_epoch2.pt'
     model = AutoModel.from_pretrained("intfloat/multilingual-e5-large")
     tokenizer = AutoTokenizer.from_pretrained("intfloat/multilingual-e5-large")
     print(f"Loading state dict from {save_path}")
@@ -25,9 +25,12 @@ def main():
     login(token=token)
     print(whoami())
 
+    # save before uploading. Was this the error the whole time?
+    model.save_pretrained("models/e5_finetuned_epoch2")
+
     # Upload to the hub
-    model.push_to_hub("E5_finetuned_epoch7")
-    tokenizer.push_to_hub("E5_finetuned_epoch7")
+    model.push_to_hub("e5_finetuned_epoch2")
+    tokenizer.push_to_hub("e5_finetuned_epoch2")
 
 if __name__ == "__main__":
     main()
