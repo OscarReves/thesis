a933e77ca80e737fa3447cdb5b6afa19f3974e94
oscar <oscar.reves@gmail.com>
2025-06-01 18:15:09 +0200

    Exploratory stats

--- scripts/wiki/count_articles.py
+++ scripts/wiki/count_articles.py
@@ -1,4 +1,14 @@
-from src.utils import load_documents_from_directory
+from src.utils import load_knowledge_base
 
 if __name__ == "__main__":
-    load_documents_from_directory('/dtu/p1/oscrev/wiki/dump')
+    kb = load_knowledge_base('/dtu/p1/oscrev/wiki/chunked_paragraph',type='wiki')
+    
+    document_count = len(kb)
+    sum = 0
+    for chunk in kb:
+        sum += len(chunk['text'].split())
+
+    mean = sum/document_count
+
+    print(f"The knowledge base contain {document_count} chunks")
+    print(f"Average length: {mean}")
