821cf64ca285652042a575562416080aa6c84319
oscar <oscar.reves@gmail.com>
2025-06-02 04:17:06 +0200

    E5-largfe

--- src/embedder/factory.py
+++ src/embedder/factory.py
@@ -1,11 +1,12 @@
-from .embedders import GPT2Embedder, E5Embedder, BertTinyEmbedder, E5EmbedderLocal, E5Finetuned
+from .embedders import GPT2Embedder, E5Embedder, BertTinyEmbedder, E5EmbedderLocal, E5Finetuned, E5Large
 
 EMBEDDER_REGISTRY = {
     "gpt2"  : GPT2Embedder,
     "e5"    : E5Embedder,
     "bert-tiny" : BertTinyEmbedder,
     "e5-local" : E5EmbedderLocal,
-    "e5-finetuned" : E5Finetuned
+    "e5-finetuned" : E5Finetuned,
+    "e5-large" : E5Large
 }
 
 def get_embedder(name, **kwargs):
